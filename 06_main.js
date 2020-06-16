63f4ba89badaaa667ec7921336fca837cd4d0839
moejoe3 <61524594+moejoe3@users.noreply.github.com>
2020-06-16 12:58:28 +0200

    Update 06_main.js

--- 06_main.js
+++ 06_main.js
@@ -30,7 +30,7 @@ $("document").ready(function() {
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
+            deployMethod: "directLink",
             contact_email: "johaupt@uni-osnabrueck.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
